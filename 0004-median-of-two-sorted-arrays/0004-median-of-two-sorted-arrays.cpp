4f204df95a1c9e8aeeb9b59b8a00e6b198c7b67d
vishalkumar0003 <vishalkumar683556@gmail.com>
2026-09-22 00:19:24 +0530

    Time: 0 ms (100%), Space: 95.1 MB (79.47%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
-        int n1 = nums1.size(),n2 = nums2.size();
-        vector<int> ans;
-        int i = 0, j = 0;
-        while(i < n1 && j <n2){
-            if(nums1[i] < nums2[j]){
-                ans.push_back(nums1[i]);
-                i++;
-            }
-            else{
-                ans.push_back(nums2[j]);
-                j++;
-            }
-        }
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        if(n1 > n2)
+        return findMedianSortedArrays(nums2, nums1);
+        int low = 0, high = n1;
+        int left = (n1+n2+1)/2;
+        int n = n1+n2;
+        while(low <= high){
+            int mid1 = (low+high)>>1;
+            int mid2 = left-mid1;
+            int l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
+            if(mid1-1 >= 0) l1 = nums1[mid1-1];
+            if(mid2-1 >= 0) l2 = nums2[mid2-1];
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
+            if(l1 <= r2 && l2 <= r1){
+                if(n%2 == 1)
+                return max(l1,l2);
+                return (double)(max(l1,l2)+min(r1,r2))/2;
 
-        while(i < n1){
-            ans.push_back(nums1[i]);
-            i++;
+            }
+            else if(l1 > r2){high = mid1-1;}
+            else low = mid1+1;
         }
-        while(j < n2){
-            ans.push_back(nums2[j]);
-            j++;
-        }
-        int mid = ans.size()/2;
-        if(ans.size()%2 == 1){
-            return (double)ans[mid];
-        }
-        double mid1 = ((double)ans[mid]+(double)ans[mid-1])/2;
-        return mid1;
+        return 0;
     }
 };

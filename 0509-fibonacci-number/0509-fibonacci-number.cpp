94e5010bdda7d8a06ba49efa26e750f38f7fa69e
vishalkumar0003 <vishalkumar683556@gmail.com>
2026-09-25 00:37:01 +0530

    Time: 13 ms (31.54%), Space: 7.8 MB (37.66%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
-
     int fib(int n) {
-      if(n<=1){
+        if(n<=1)
         return n;
-      }  
-      return fib(n-1)+fib(n-2);
+        return fib(n-1)+fib(n-2);
+
     }
 };

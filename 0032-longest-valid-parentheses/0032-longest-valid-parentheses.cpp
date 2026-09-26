81ce1941b7510e8be9e2d987a3052d4f751c4257
vishalkumar0003 <vishalkumar683556@gmail.com>
2026-09-26 23:02:31 +0530

    Time: 8 ms (8.69%), Space: 11.6 MB (71.75%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -3,26 +3,18 @@ public:
     int longestValidParentheses(string s) {
         stack<int> st;
         st.push(-1);
-
         int ans = 0;
-
-        for (int i = 0; i < s.size(); i++) {
-
-            if (s[i] == '(') {
+        for(int i = 0;i < s.size();i++){
+            if(s[i] == '('){
                 st.push(i);
-            }
-            else {
+            }else{
                 st.pop();
-
-                if (st.empty()) {
-                    st.push(i);
-                }
-                else {
-                    ans = max(ans, i - st.top());
-                }
+                if(st.empty())
+                st.push(i);
+                else
+                ans = max(ans, i-st.top());
             }
         }
-
         return ans;
     }
 };
